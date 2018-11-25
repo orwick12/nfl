5af462f6750ae905a8e7100ca27c1daebfb87bf4
Brian Orwick <Orwick12@outlook.com>
2018-11-25 09:09:24 -0500

    QB and RB data is being inserted and sorted from player data. need to add inserts for all other positions

--- src/backEnd/DB.py
+++ src/backEnd/DB.py
@@ -42,7 +42,7 @@ class DB(object):
                                                               "PLAYER text NOT NULL, "
                                                               "TEAM text NOT NULL, POS text NOT NULL, "
                                                               "ATT INTEGER NOT NULL, ATTPERGAME FLOAT NOT NULL, "
-                                                              "YDS INTEGER NOT NULL, AVG FLOAT NOT NULL, "
+                                                              "YDS INTEGER NOT NULL, AVERAGE FLOAT NOT NULL, "
                                                               "YARDSPERGAME FLOAT NOT NULL, TD INTEGER NOT NULL, "
                                                               "LNG INTEGER NOT NULL, FIRSTDOWN INTEGER NOT NULL, "
                                                               "FIRSTDOWNPCT FLOAT NOT NULL, "
@@ -53,7 +53,7 @@ class DB(object):
                                                                "PLAYER text NOT NULL, "
                                                                "TEAM text NOT NULL, POS text NOT NULL, "
                                                                "REC INTEGER NOT NULL, YDS INTEGER NOT NULL, "
-                                                               "AVG FLOAT NOT NULL, YARDSPERGAME FLOAT NOT NULL,"
+                                                               "AVERAGE FLOAT NOT NULL, YARDSPERGAME FLOAT NOT NULL,"
                                                                "LNG INTEGER NOT NULL, TD INTEGER NOT NULL, "
                                                                "TWENTYPLUS INTEGER NOT NULL, FORTYPLUS INTEGER NOT NULL,"
                                                                "FIRSTDOWN INTEGER NOT NULL, FIRSTDOWNPERCENT FLOAT NOT NULL,"
@@ -63,7 +63,7 @@ class DB(object):
                                                            "PLAYER text NOT NULL,"
                                                            "TEAM text NOT NULL, POS text NOT NULL, "
                                                            "REC INTEGER NOT NULL, YDS INTEGER NOT NULL, "
-                                                           "AVG FLOAT NOT NULL, YARDSPERGAME FLOAT NOT NULL, "
+                                                           "AVERAGE FLOAT NOT NULL, YARDSPERGAME FLOAT NOT NULL, "
                                                            "LNG INTEGER NOT NULL, TD INTEGER NOT NULL, "
                                                            "TWENTYPLUS INTEGER NOT NULL, FORTYPLUS INTEGER NOT NULL, "
                                                            "FIRSTDOWN INTEGER NOT NULL, FIRSTDOWNPERCENT FLOAT NOT NULL,"
@@ -89,11 +89,11 @@ class DB(object):
         conn.commit()
         conn.close()
 
-    def db_rb_insert(self, player, team):
+    def db_rb_insert(self, player, team, pos, att, attpergame, yds, average, yardspergame, td, lng, firstdown, firstdownpct, twentyplus, fortyplus, fumble):
         conn = sqlite3.connect(self.dbFile)
         c = conn.cursor()
-        c.execute("INSERT INTO {tn} (PLAYER, TEAM) VALUES (?, ?)".format(tn=self.table_runningbacks),
-                  (player, team))
+        c.execute("INSERT INTO {tn} (PLAYER, TEAM, POS, ATT, ATTPERGAME, YDS, AVERAGE, YARDSPERGAME, TD, LNG, FIRSTDOWN, FIRSTDOWNPCT, TWENTYPLUS, FORTYPLUS, FUMBLE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(tn=self.table_runningbacks),
+                  (player, team, pos, att, attpergame, yds, average, yardspergame, td, lng, firstdown, firstdownpct, twentyplus, fortyplus, fumble))
         conn.commit()
         conn.close()
 
--- src/backEnd/Scraper.py
+++ src/backEnd/Scraper.py
@@ -29,7 +29,7 @@ class Scraper(object):
                     if player[3] == "QB":
                         self.db.db_qb_insert(player=player[1], team=player[2], pos=player[3], comp=player[4], att=player[5], pct=player[6], attpergame=player[7], yds=player[8], avg=player[9], yardspergame=player[10], td=player[11], interception=player[12], firstdown=player[13], firstdownpct=player[14], lng=player[15], twentyplus=player[16], fortyplus=player[17], sack=player[18], rate=player[19])
                     elif player[3] == "RB":
-                        self.db.db_rb_insert()
+                        self.db.db_rb_insert(player=player[1], team=player[2],pos=player[3], att=player[4], attpergame=player[5], yds=player[6], average=player[7], yardspergame=player[8], td=player[9], lng=player[10], firstdown=player[11], firstdownpct=player[12], twentyplus=player[13], fortyplus=player[14], fumble=player[15])
                     elif player[3] == "WR":
                         self.db.db_wr_insert()
                     elif player[3] == "TE":
