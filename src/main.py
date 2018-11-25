50e3aa4ba202fa8efb0579b429f337bd8171e18c
Brian Orwick <Orwick12@outlook.com>
2018-11-25 08:39:12 -0500

    QB data is being inserted and sorted from player data. need to add inserts for all other positions

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from backEnd.Scraper import Scraper
 from backEnd.DB import DB
 
 websites = [
-    'http://www.nfl.com/stats/categorystats?tabSeq=1&statisticPositionCategory=QUARTERBACK&season=2018&seasonType=REG',
-    'http://www.nfl.com/stats/categorystats?tabSeq=1&statisticPositionCategory=RUNNING_BACK&season=2018&seasonType=REG',]
+    'http://www.nfl.com/stats/categorystats?tabSeq=1&statisticPositionCategory=QUARTERBACK&season=2018&seasonType=REG',]
+    # 'http://www.nfl.com/stats/categorystats?tabSeq=1&statisticPositionCategory=RUNNING_BACK&season=2018&seasonType=REG',
     # 'http://www.nfl.com/stats/categorystats?tabSeq=1&statisticPositionCategory=WIDE_RECEIVER&season=2018&seasonType=REG',
     # 'http://www.nfl.com/stats/categorystats?tabSeq=1&statisticPositionCategory=TIGHT_END&season=2018&seasonType=REG',
     # 'http://www.nfl.com/stats/categorystats?tabSeq=1&statisticPositionCategory=FIELD_GOAL_KICKER&season=2018&seasonType=REG']
